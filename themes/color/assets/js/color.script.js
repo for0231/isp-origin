3d02f38d6d777c5c57a04e0d93e22959c1daf236
John <John-lee@2001594.no-reply.drupal.org>
2018-06-15 21:11:56 +0800

    fix.

---
+++ themes/color/assets/js/color.script.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ */
+(function ($, Drupal, window, document, undefined) {
+
+  // Hello World.
+  Drupal.behaviors.helloWorld = {
+    attach: function (context) {
+      console.log('Hello World');
+    }
+  }
+
+
+
+
+
+})(jQuery, Drupal, this, this.document);
